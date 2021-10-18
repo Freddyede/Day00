da9a8cbd25f6687d582daa12d0b77c21f0048f95
Franck <patouillardfranck3@gmail.com>
2021-10-18 16:40:45 +0200

    end exercice 5

--- it_alumni.sh
+++ it_alumni.sh
@@ -7,4 +7,6 @@
 awk -F , '$5=="PHP" && $2 =="Remote" && $1=="2019" {count++} END {printf("%d", count)}' it_alumni.csv > php_remote_2019.csv;
 awk -F , '($2 =="Charbonnieres" || $2=="Villeurbanne") && $5=="Js" {print}' it_alumni.csv > javascript_villeurbanne_charbonnieres.csv;
 awk -F , '$3=="David" || $4=="David"{print}' it_alumni.csv > david.csv;
-awk -F , '$2=="Gex" {mail= $6} END {printf("%s", count)}' it_alumni.csv > mails_gex.csv;
+awk -F , '$2=="Gex" {print $6}' it_alumni.csv > mails_gex.csv;
+awk -F , '$2=="Charbonnieres" || $2=="Remote"  {print $6}' it_alumni.csv > mails_charbonnieres_remote.csv;
+awk -F , '$2=="Charbonnieres" && $5=="Python" && $1=="2008" {count++} END {printf("%d", count)}' it_alumni.csv > python_2008.csv;
